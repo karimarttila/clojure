82be76cefdd39f5f8596d764292407e577bd6100
Kari Marttila <kari.marttila@iki.fi>
2019-01-25 15:46:10 +0200

    Note regarding odd behaviour regarding environment variable order

--- clj-ring-cljs-reagent-demo/simple-server/start-simple-server-without-lein-env.sh
+++ clj-ring-cljs-reagent-demo/simple-server/start-simple-server-without-lein-env.sh
@@ -3,5 +3,9 @@
 # First move .lein-env away so that you know environmental variables should work.
 # You should also build the single-node version!
 
+# First move the .lein-env file so that we are not using it.
 mv .lein-env tmp/.
+
+# Then start the server.
+# TODO: For some reason the MY_ENV needs to be before SS_ENV - why: investigate!
 SIMPLESERVER_CONFIG_FILE=resources/simpleserver.properties;MY_ENV=dev;SS_ENV=single-node java -jar target/uberjar/simple-server-1.0-standalone.jar
