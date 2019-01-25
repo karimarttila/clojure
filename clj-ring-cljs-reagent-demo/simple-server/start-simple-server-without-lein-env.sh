b307c051dfe7e1e39a1489861900dc4414f94097
Kari Marttila <kari.marttila@iki.fi>
2019-01-25 16:04:35 +0200

    Some modifications to the start server script

--- clj-ring-cljs-reagent-demo/simple-server/start-simple-server-without-lein-env.sh
+++ clj-ring-cljs-reagent-demo/simple-server/start-simple-server-without-lein-env.sh
@@ -7,5 +7,15 @@
 mv .lein-env tmp/.
 
 # Then start the server.
+
+export SS_ENV="single-node"
+export MY_ENV="dev"
+export SIMPLESERVER_CONFIG_FILE="resources/simpleserver.properties"
+
+echo $SS_ENV
+echo $MY_ENV
+echo $SIMPLESERVER_CONFIG_FILE
+java -jar target/uberjar/simple-server-1.0-standalone.jar
+
 # TODO: For some reason the MY_ENV needs to be before SS_ENV - why: investigate!
-SIMPLESERVER_CONFIG_FILE=resources/simpleserver.properties;MY_ENV=dev;SS_ENV=single-node java -jar target/uberjar/simple-server-1.0-standalone.jar
+# SIMPLESERVER_CONFIG_FILE=resources/simpleserver.properties;MY_ENV=dev;SS_ENV=single-node java -jar target/uberjar/simple-server-1.0-standalone.jar
