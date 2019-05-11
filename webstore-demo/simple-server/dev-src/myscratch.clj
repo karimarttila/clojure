83a22bfd71c35c11c1d548a9894762c27f94684a
Kari Marttila <kari.marttila@iki.fi>
2019-05-11 11:36:57 +0300

    Domain single node ready

--- webstore-demo/simple-server/dev-src/myscratch.clj
+++ webstore-demo/simple-server/dev-src/myscratch.clj
@@ -5,6 +5,35 @@
 ;; Store general Clojure scratch code snippets in: /mnt/edata/aw/kari/my-clj-dev/dev-src/commonscratch.clj
 ;; ... and rename that file occasionally to e.g. "old-scratch-2019-05" in that directory.
 
+(in-ns 'user)
+
+(mydev/reset)
+
+
+(require '[simpleserver.domain.domain-single-node])
+
+(def s-node (simpleserver.domain.domain-single-node/->SingleNodeR simpleserver.util.config/config-state))
+s-node
+(simpleserver.domain.domain-interface/get-product-groups s-node)
+(simpleserver.domain.domain-interface/get-products s-node 1)
+(simpleserver.domain.domain-interface/get-product s-node 1 2024)
+
+
+
+(simpleserver.domain.domain-single-node/-get-raw-products 1)
+
+@simpleserver.domain.domain-single-node/my-domain-atom
+
+(def pg-id 1)
+(require '[clojure.java.io :as io])
+(require '[clojure.data.csv :as csv])
+(require '[simpleserver.util.config :as ss-config])
+(def data-dir (get-in simpleserver.util.config/config-state [:domain :data-dir]))
+data-dir
+(with-open [reader (io/reader (str data-dir "/pg-" pg-id "-products.csv"))]
+                (doall
+                  (csv/read-csv reader :separator \tab)))
+
 ; Get vars in user namespace.
 (ns-publics 'user)
 
@@ -53,3 +82,4 @@ my-cs
 
 
 (mydev/curl-get "/info")
+
--- webstore-demo/simple-server/src/simpleserver/domain/domain_interface.clj
+++ webstore-demo/simple-server/src/simpleserver/domain/domain_interface.clj
@@ -1,1 +1,12 @@
 (ns simpleserver.domain.domain-interface)
+
+
+(defprotocol DomainInterface
+  (get-product-groups [env]
+    "Gets product groups")
+  (get-products [env pg-id]
+    "Gets products for a product group, returns list of items: [p-id, pg-id, name, price]")
+  (get-product [env pg-id p-id]
+    "Gets product info for a product, returned item varies related to product group")
+  )
+
