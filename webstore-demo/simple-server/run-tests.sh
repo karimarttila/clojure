f3435c6183cf7d529c31cc82122d8c27e50a37dd
Kari Marttila <kari.marttila@iki.fi>
2019-07-19 13:32:21 +0300

    Added configurations to hit different db backends depending on the given environment as test runner script parameter

---
+++ webstore-demo/simple-server/run-tests.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+if [ $# -ne 1 ]
+then
+    echo "Usage: ./run-tests-dev-env.sh <configuration>"
+    echo "Configurations: env-dev, env-dev-single-node, env-dev-local-dynamodb, env-dev-real-aws"
+    echo "Example: ./run-tests.sh env-dev-single-node"
+    exit 1
+fi
+
+MYENV=$1
+
+clj -Atest-runner:${MYENV}
+
+# For more examples how to run different tests, exclude some etc:
+# https://github.com/cognitect-labs/test-runner
+
