3377f9b603b52d8e8423c139824f811860f4498e
Kari Marttila <kari.marttila@iki.fi>
2020-09-21 18:03:55 +0300

    Based the main.cljs on https://github.com/metosin/reitit/tree/master/examples/frontend-re-frame

--- webstore-demo/re-frame-demo/src/cljs/simplefrontend/main.cljs
+++ webstore-demo/re-frame-demo/src/cljs/simplefrontend/main.cljs
@@ -1,99 +1,138 @@
 (ns simplefrontend.main
-  (:require
-    [re-frame.core :as re-frame]
-    [reagent.core :as r]
-    [reagent.dom]
-    [re-frame.db]
-    [reitit.frontend :as rf]
-    [reitit.frontend.controllers :as rfc]
-    [reagent-dev-tools.core :as dev-tools]
-    [reitit.frontend.easy :as rfe]
-    [simplefrontend.state :as state]
-    [simplefrontend.signin :as signin]
-    ))
+  (:require [re-frame.core :as re-frame]
+            [reagent.core :as reagent]
+            [reitit.core :as r]
+            [reitit.coercion.spec :as rss]
+            [reitit.frontend :as rf]
+            [reitit.frontend.controllers :as rfc]
+            [reitit.frontend.easy :as rfe]))
 
-(re-frame/reg-event-db ::initialize-db
-                       (fn [_ _] {:current-route nil
-                                  :loading 0}))
+;;; Events ;;;
 
-(re-frame/reg-event-db ::navigated
-                       (fn [db [_ new-match]]
-                         (let [old-match (:current-route db)
-                               controllers (rfc/apply-controllers (:controllers old-match) new-match)]
-                           (-> db
-                               (assoc :current-route (assoc new-match :controllers controllers))))))
+(re-frame/reg-event-db
+ ::initialize-db
+ (fn [_ _]
+   {:current-route nil}))
 
-(re-frame/reg-sub ::current-route
-                  (fn [db] (:current-route db)))
+(re-frame/reg-event-fx
+ ::navigate
+ (fn [db [_ & route]]
+   ;; See `navigate` effect in routes.cljs
+   {::navigate! route}))
 
-(re-frame/reg-sub ::data
-                  (fn [db [_]]
-                    (get-in db [:data])))
+(re-frame/reg-event-db
+ ::navigated
+ (fn [db [_ new-match]]
+   (let [old-match   (:current-route db)
+         controllers (rfc/apply-controllers (:controllers old-match) new-match)]
+     (assoc db :current-route (assoc new-match :controllers controllers)))))
 
-(re-frame/reg-fx ::navigate!
-                 (fn [route]
-                   (apply rfe/push-state route)))
+;;; Subscriptions ;;;
 
-(re-frame/reg-event-fx :navigate
-                       (fn [_ [_ & route]]
-                         {::navigate! route}))
+(re-frame/reg-sub
+ ::current-route
+ (fn [db]
+   (:current-route db)))
 
-(re-frame/reg-event-fx :fx
-                       (fn [_ [_ fx]] fx))
+;;; Views ;;;
 
-
-(defn home-page
-  []
+(defn home-page []
   [:div
-   [:h1 "Web Store"]
-   [:a {:href "#/signin"} "Sign-in"]
-   [:div]
-   [:a {:href "#/login"} "Login"]
-   [:div]
-   [:a {:href "#/productgroups"} "Product Groups"]])
+   [:h1 "This is home page"]
+   [:button
+    ;; Dispatch navigate event that triggers a (side)effect.
+    {:on-click #(re-frame/dispatch [::navigate ::sub-page2])}
+    "Go to sub-page 2"]])
 
-(defn main-view [{:keys [_]}]
-  (let [current-route @(re-frame/subscribe [::current-route])
-        view (-> current-route :data :view)]
-    (if view
-      [home-page]
-      )))
+(defn sub-page1 []
+  [:div
+   [:h1 "This is sub-page 1"]])
 
-(re-frame/reg-event-fx :http
-                       (fn [_ [_ request]]
-                         (js/console.log "reg-event-fx :http")))
+(defn sub-page2 []
+  [:div
+   [:h1 "This is sub-page 2"]])
 
-(defn dispatch [re-frame-event]
-  (comp
-    (re-frame/dispatch re-frame-event)
-    ))
+;;; Effects ;;;
 
-(def fe-controller
-  {:start (fn []
-            (dispatch
-              [:http {:method :get
-                      :uri "/api/ping"
-                      :on-success [::state/todo]}]))})
+;; Triggering navigation from events.
+(re-frame/reg-fx
+ ::navigate!
+ (fn [route]
+   (apply rfe/push-state route)))
+
+;;; Routes ;;;
+
+(defn href
+  "Return relative url for given route. Url can be used in HTML links."
+  ([k]
+   (href k nil nil))
+  ([k params]
+   (href k params nil))
+  ([k params query]
+   (rfe/href k params query)))
+
+(def routes
+  ["/"
+   [""
+    {:name      ::home
+     :view      home-page
+     :link-text "Home"
+     :controllers
+     [{;; Do whatever initialization needed for home page
+       ;; I.e (re-frame/dispatch [::events/load-something-with-ajax])
+       :start (fn [& params](js/console.log "Entering home page"))
+       ;; Teardown can be done here.
+       :stop  (fn [& params] (js/console.log "Leaving home page"))}]}]
+   ["sub-page1"
+    {:name      ::sub-page1
+     :view      sub-page1
+     :link-text "Sub page 1"
+     :controllers
+     [{:start (fn [& params] (js/console.log "Entering sub-page 1"))
+       :stop  (fn [& params] (js/console.log "Leaving sub-page 1"))}]}]
+   ["sub-page2"
+    {:name      ::sub-page2
+     :view      sub-page2
+     :link-text "Sub-page 2"
+     :controllers
+     [{:start (fn [& params] (js/console.log "Entering sub-page 2"))
+       :stop  (fn [& params] (js/console.log "Leaving sub-page 2"))}]}]])
+
+(defn on-navigate [new-match]
+  (when new-match
+    (re-frame/dispatch [::navigated new-match])))
 
 (def router
   (rf/router
-    ["/" {:controllers [fe-controller]}
-
-     ["" {:name :main-view
-          :view main-view}]
-     ]))
-
-;#_ ["sign-in" {:name :sign-in
-;                 :view signin/signin-page}
-;      ]
+   routes
+   {:data {:coercion rss/coercion}}))
 
 (defn init-routes! []
   (js/console.log "initializing routes")
   (rfe/start!
-    router
-    (fn [match] (when match (re-frame/dispatch [::navigated match])))
-    {:use-fragment true}))
+   router
+   on-navigate
+   {:use-fragment true}))
 
+(defn nav [{:keys [router current-route]}]
+  [:ul
+   (for [route-name (r/route-names router)
+         :let       [route (r/match-by-name router route-name)
+                     text (-> route :data :link-text)]]
+     [:li {:key route-name}
+      (when (= route-name (-> current-route :data :name))
+        "> ")
+      ;; Create a normal links that user can click
+      [:a {:href (href route-name)} text]])])
+
+(defn router-component [{:keys [router]}]
+  (let [current-route @(re-frame/subscribe [::current-route])]
+    [:div
+     [nav {:router router :current-route current-route}]
+     (when current-route
+       [(-> current-route :data :view)])]))
+
+;;; Setup ;;;
 
 (def debug? ^boolean goog.DEBUG)
 
@@ -102,23 +141,13 @@
     (enable-console-print!)
     (println "dev mode")))
 
-(defn ^:dev/after-load start []
-  (js/console.log "start")
+(defn mount-root []
   (re-frame/clear-subscription-cache!)
-  (init-routes!)
-  (reagent.dom/render
-    [main-view {:router router}
-     (if (:open? @dev-tools/dev-state)
-       {:style {:padding-bottom (str (:height @dev-tools/dev-state) "px")}})]
-    (.getElementById js/document "app")))
+  (init-routes!) ;; Reset routes on figwheel reload
+  (reagent/render [router-component {:router router}]
+                  (.getElementById js/document "app")))
 
 (defn ^:export init []
   (re-frame/dispatch-sync [::initialize-db])
-  (dev-tools/start! {:state-atom re-frame.db/app-db})
   (dev-setup)
-  (js/console.log "init")
-  (start))
-
-(defn ^:dev/before-load stop []
-  (js/console.log "stop"))
-
+  (mount-root))
