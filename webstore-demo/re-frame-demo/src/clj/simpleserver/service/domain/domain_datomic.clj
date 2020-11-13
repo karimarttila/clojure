1192cb8efaaaee7661cd745e5516e0b5b767e106
Kari Marttila <kari.marttila@iki.fi>
2020-11-13 21:10:30 +0200

    Datomic domain layer is ready

--- webstore-demo/re-frame-demo/src/clj/simpleserver/service/domain/domain_datomic.clj
+++ webstore-demo/re-frame-demo/src/clj/simpleserver/service/domain/domain_datomic.clj
@@ -11,20 +11,43 @@
     [_ _]
     (log/debug "ENTER get-product-groups")
     (let [raw-pg (d/q '[:find ?id ?name
-                              :where [?e :domain.product-group/id ?id]
-                              [?e :domain.product-group/name ?name]]
-                            (d/db conn))]
+                        :where [?e :domain.product-group/id ?id]
+                        [?e :domain.product-group/name ?name]]
+                      (d/db conn))]
       (reduce (fn [acc [id name]]
-            (assoc acc (str id) name))
-          {}
-       raw-pg)))
+                (assoc acc (str id) name))
+              {}
+              raw-pg)))
 
   (get-products
     [_ _ pg-id]
     (log/debug (str "ENTER get-products, pg-id: " pg-id))
-    )
+    (let [raw-products (d/q '[:find ?id ?pg-id ?title ?price
+                              :in $ ?pg-id
+                              :where
+                              [?e :domain.product/id ?id]
+                              [?e :domain.product/pg-id ?pg-id]
+                              [?e :domain.product/title ?title]
+                              [?e :domain.product/price ?price]]
+                            (d/db conn) pg-id)]
+      (map (fn [[p-id pg-id title price]]
+         [(str p-id) (str pg-id) title (str price)]) raw-products)))
 
   (get-product
     [_ _ pg-id p-id]
     (log/debug (str "ENTER get-product, pg-id: " pg-id ", p-id: " p-id))
-    ))
+    (let [raw-product (d/q '[:find ?id ?pg-id ?title ?price ?a_or_d ?year ?country ?g_or_l
+                             :in $ ?pg-id ?id
+                             :where
+                             [?e :domain.product/id ?id]
+                             [?e :domain.product/pg-id ?pg-id]
+                             [?e :domain.product/title ?title]
+                             [?e :domain.product/price ?price]
+                             [?e :domain.product/a_or_d ?a_or_d]
+                             [?e :domain.product/year ?year]
+                             [?e :domain.product/country ?country]
+                             [?e :domain.product/g_or_l ?g_or_l]]
+                           (d/db conn) pg-id p-id)]
+      (first (map (fn [[id pg-id title price a_or_d year country g_or_l]]
+                    [(str id) (str pg-id) title (str price) a_or_d (str year) country g_or_l])
+                  raw-product)))))
