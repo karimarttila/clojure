3c780e81a9128fc149fc2ee8188fd83204f11dec
Kari Marttila <kari.marttila@iki.fi>
2020-11-13 20:06:22 +0200

    Added domain get-product-groups for Datomic

--- webstore-demo/re-frame-demo/src/clj/simpleserver/service/domain/domain_datomic.clj
+++ webstore-demo/re-frame-demo/src/clj/simpleserver/service/domain/domain_datomic.clj
@@ -10,7 +10,14 @@
   (get-product-groups
     [_ _]
     (log/debug "ENTER get-product-groups")
-    )
+    (let [raw-pg (d/q '[:find ?id ?name
+                              :where [?e :domain.product-group/id ?id]
+                              [?e :domain.product-group/name ?name]]
+                            (d/db conn))]
+      (reduce (fn [acc [id name]]
+            (assoc acc (str id) name))
+          {}
+       raw-pg)))
 
   (get-products
     [_ _ pg-id]
