d3b4682f21e98c8c51e8f03c6c360f0c0cb3ab9b
Kari Marttila <kari.marttila@iki.fi>
2020-12-24 15:19:12 +0200

    First version of worldstat frontend, not yet working

---
+++ statistics/worldstat/src/cljs/worldstat/frontend/main.cljs
@@ -0,0 +1,14 @@
+(ns worldstat.frontend.main)
+
+
+(ns worldstat.frontend.main
+  (:require [re-frame.core :as re-frame]
+            [re-frame.db]
+            [reagent.dom :as r-dom]
+            [day8.re-frame.http-fx] ; Needed to register :http-xhrio to re-frame.
+            [reagent-dev-tools.core :as dev-tools]
+            [reitit.coercion.spec :as rss]
+            [reitit.frontend :as rf]
+            [reitit.frontend.controllers :as rfc]
+            [reitit.frontend.easy :as rfe]
+            ))
