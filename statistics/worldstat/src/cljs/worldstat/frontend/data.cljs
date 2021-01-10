c1012de345648769dd69d5f1a90b193fa45c733d
Kari Marttila <kari.marttila@iki.fi>
2021-01-10 18:53:02 +0200

    Minor refactorings

--- statistics/worldstat/src/cljs/worldstat/frontend/data.cljs
+++ statistics/worldstat/src/cljs/worldstat/frontend/data.cljs
@@ -17,9 +17,7 @@
      :projection {:type "mercator"}
      :mark {:type "geoshape"}
      :encoding {:color {:field "value"
-                        :type "quantitative"}
-                }
-     })
+                        :type "quantitative"}}})
 
 (defn world-schema [points year metric-name]
   {:schema "https://vega.github.io/schema/vega/v5.json"
@@ -35,7 +33,6 @@
               :title metric-name
               :titleAnchor "start"
               :titleOrient "bottom"
-
               :format "0"}]
    :marks [{:type "shape"
             :from {:data "graticule"}
@@ -44,60 +41,33 @@
                               :stroke {:signal "invert ? '#444' : '#ddd'"}
                               :fill {:value nil}}}
             :transform [{:type "geoshape" :projection "projection"}]}
-
-           {:type "shape"
-            :from {:data "world"}
-            :encode {:color {:field "value" :type "quantitative"}
-                     :update {:strokeWidth {:signal "+borderWidth"}
-                              :stroke {:signal "invert ? '#777' : '#bbb'"}
-                              :fill {:signal "invert ? '#fff' : '#000'"}
-                              :zindex {:value 0}}
-                     :hover {:strokeWidth {:signal "+borderWidth + 1"}
-                             :stroke {:value "firebrick"}
-                             :zindex {:value 1}}}
-            :transform [{:type "geoshape"
-                         :projection "projection"
-                         }]}
-
            {:type "shape"
             :from {:data "world"}
             :encode {:enter
-                     {:tooltip
-                      {:signal "format(datum.value, '0.1')"}},
-                     :update
-                     {:fill {:scale "color", :field "value"}},
+                     {:tooltip {:signal "format(datum.value, '0.1')"}},
+                     :update {:fill {:scale "color", :field "value"}},
                      :hover {:fill {:value "red"}}},
             :transform [{:type "geoshape"
                          :lookup "id"
                          :from {:data {:values (transduce (ws-filter/filter-by-year year) conj points)}
                                 :key "country-id"
                                 :fields ["value"]}
-                         :projection "projection"
-                         }
-                        ]}
-           ]
+                         :projection "projection"}]}]
    :projections [{:name "projection"
                   :type {:signal "type"}
                   :scale {:signal "scale"}
                   :rotate [{:signal "rotate0"} {:signal "rotate1"} {:signal "rotate2"}]
                   :center [{:signal "center0"} {:signal "center1"}]
                   :translate [{:signal "translate0"} {:signal "translate1"}]}]
-   :signals [{:name "type"
-              :value "mercator"}
-             {:name "scale"
-              :value 150
+   :signals [{:name "type" :value "mercator"}
+             {:name "scale" :value 150
               :bind {:input "range" :min 50 :max 2000 :step 1}}
-             {:name "rotate0"
-              :value -8
+             {:name "rotate0" :value -8
               :bind {:input "range" :min -180 :max 180 :step 1}}
-             {:name "rotate1"
-              :value 0}
-             {:name "rotate2"
-              :value 0}
-             {:name "center0"
-              :value 0}
-             {:name "center1"
-              :value 23}
+             {:name "rotate1" :value 0}
+             {:name "rotate2" :value 0}
+             {:name "center0" :value 0}
+             {:name "center1" :value 23}
              {:name "translate0" :update "width / 2"}
              {:name "translate1" :update "height / 2"}
              {:name "graticuleDash" :value 0}
@@ -105,8 +75,7 @@
              {:name "background" :value "#ffffff"}
              {:name "invert" :value false}]
    :data [{:name "countries"
-           :values (transduce (ws-filter/filter-by-year year) conj points)
-           }
+           :values (transduce (ws-filter/filter-by-year year) conj points)}
           {:name "world"
            :url "data/world-110m.json"
            :format {:type "topojson" :feature "countries"}
@@ -114,8 +83,7 @@
                         :from "countries"
                         :key "country-id"
                         :fields ["id"]
-                        :values ["value" "country-name"]
-                        }]}
+                        :values ["value" "country-name"]}]}
           {:name "graticule"
            :transform [{:type "graticule"}]}]})
 
