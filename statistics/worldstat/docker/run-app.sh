40269c5066a8acbfcc86406f7d81d8030027edcb
Kari Marttila <kari.marttila@iki.fi>
2021-01-18 23:00:53 +0200

    Tuned JVM memory settings more to not to exceel Heroku 512MB memory limi

--- statistics/worldstat/docker/run-app.sh
+++ statistics/worldstat/docker/run-app.sh
@@ -5,8 +5,12 @@ set -e
 export PROFILE=prod
 
 # Replace the parent process (the run-app.sh script) with this command to make java process pid 1.
+# Memory limits are pretty harsh because of the Heroku free tier memory limit (512MB)
 exec java \
     -server \
-    -Xms300m \
-    -Xmx300m \
+    -Xms180m \
+    -Xmx250m \
+    -Xss512k \
+    -XX:+UseContainerSupport \
+    -XX:CICompilerCount=2 \
     -cp /app/worldstat.jar clojure.main --report stderr -m worldstat.backend.main
