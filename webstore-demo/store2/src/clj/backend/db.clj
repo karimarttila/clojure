76b95880812a49405634fb1a6c1d20c9ede99b3f
Kari Marttila <kari.marttila@iki.fi>
2025-02-16 19:31:21 +0200

    db.clj reading data files is ready

---
+++ webstore-demo/store2/src/clj/backend/db.clj
@@ -0,0 +1,87 @@
+(ns backend.db
+  (:require [clojure.java.io :as io]
+            [clojure.tools.logging :as log]
+            [clojure.string :as s]
+            [integrant.core :as ig]))
+
+
+(def books-fn (fn [[id product-group title price author year country language]]
+                (zipmap [:id :product-group :title :price :author :year :country :language]
+                        [id product-group title price author year country language])))
+
+
+(def movies-fn (fn [[id product-group title price director year country genre]]
+                 (zipmap [:id :product-group :title :price :director :year :country :genre]
+                         [id product-group title price director year country genre])))
+
+
+(defn read-datafile [file f]
+  (->> file
+       (io/resource)
+       (slurp)
+       (s/split-lines)
+       (map #(clojure.string/split % #"\t"))
+       (map f))
+  )
+
+
+(defmethod ig/init-key :db/tsv [_ {:keys [path data] :as db-opts}]
+  (log/infof "Reading tsv data, config is %s" (pr-str db-opts))
+  ; TODO: Implement: Read books and movies and add them to db atom.
+  (let [books (read-datafile (str path "/" (:books data)) books-fn)
+        movies (read-datafile (str path "/" (:movies data)) movies-fn)]
+    (atom {:db {:books books
+                :movies movies}}))
+  )
+
+
+(comment
+  
+  ;; Let's leave the rich comment here,
+  ;; readers can see how I developed the functionality.
+
+  (->> (user/env)
+       keys)
+  ;;=> (:db/tsv :web/routes :adapter/jetty)
+  (->> (user/env)
+       :db/tsv
+       ; Deref atom
+       deref
+       :db
+       :movies
+       count)
+  ;;=> 169
+  (->> (user/env)
+       :db/tsv
+       ; Deref atom
+       deref
+       :db
+       :movies
+       first)
+;;=> {:id "1",
+;;    :product-group "2",
+;;    :title "Juurakon Hulda",
+;;    :price "82.92",
+;;    :director "Valentin Vaala",
+;;    :year "1937",
+;;    :country "Finland",
+;;    :genre "Drama"}
+  
+
+  (->> "data/books.tsv"
+       (clojure.java.io/resource)
+       (slurp)
+       (clojure.string/split-lines)
+       (map #(clojure.string/split % #"\t"))
+       (map books-fn))
+
+  (->> "data/movies.tsv"
+       (clojure.java.io/resource)
+       (slurp)
+       (clojure.string/split-lines)
+       (map #(clojure.string/split % #"\t"))
+       (map movies-fn))
+
+
+
+  )
