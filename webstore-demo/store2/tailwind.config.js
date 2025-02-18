7ebd7614f1ddb5169cc4ee440744fef5878b7eea
Kari Marttila <kari.marttila@iki.fi>
2025-02-18 18:05:21 +0200

    Minor tailwind modifications

--- webstore-demo/store2/tailwind.config.js
+++ webstore-demo/store2/tailwind.config.js
@@ -4,14 +4,14 @@ module.exports = {
   // in prod look at shadow-cljs output file in dev look at runtime, which will change files that are actually compiled; postcss watch should be a whole lot faster
   //content: process.env.NODE_ENV == 'production' ? ["./public/js/main.js"] : ["./src/cljs/**/*.cljs", "./target/dev/public/js/cljs-runtime/*.js"],
   content: process.env.NODE_ENV == 'production' ? ["./public/js/main.js"] : ["./src/cljs/**/*.cljs"],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
-      },
-    },
-  },
-  plugins: [
-
-  ],
+//   theme: {
+//     extend: {
+//       fontFamily: {
+//         sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+//       },
+//     },
+//   },
+//   plugins: [
+//     require('@tailwindcss/forms'),
+//   ],
 }
