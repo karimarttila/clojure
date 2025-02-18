68ef5b5f342b43f946b71ea60e6b34df90fec049
Kari Marttila <kari.marttila@iki.fi>
2025-02-18 18:52:41 +0200

    Landing page is ready

--- webstore-demo/store2/src/cljs/frontend/core.cljs
+++ webstore-demo/store2/src/cljs/frontend/core.cljs
@@ -4,7 +4,24 @@
 
 
 (defui app []
-  ($ :h1.text-blue-700.dummy "Hello, UIx!!"))
+  ($ :div.flex.justify-center.h-screen
+     ($ :div.flex.flex-col.items-center
+        ($ :h1.mt-4.text-4xl.text-blue-800 "Web Store 2")
+        ($ :div.flex.flex-col.mt-4
+           ($ :span.text-3xl.text-blue-800 "Browse products:")
+           ($ :div.flex.flex-col.items-center.mt-4
+              ($ :a.mt-2.text-2xl.text-blue-700 {:href "#"} "Books")
+              ($ :a.mt-2.text-2xl.text-blue-700 {:href "#"} "Movies"))))))
+
+#_(defui app []
+  ($ :div.flex.justify-center.h-screen 
+     ($ :h1.text-blue-700.mt-4 "Web Store 2")
+     
+     ($ :a.text-blue-500.mt-2 {:href "#"} "Books")
+     
+     ($ :a.text-blue-500.mt-2 {:href "#"} "Movies")
+     
+     ))
 
 (defonce root
   ; NOTE: This id needs to be the same as in routes.clj => index => [:div#store2app
