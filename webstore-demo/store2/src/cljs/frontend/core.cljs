7c48f73ecee8d22651f90c12dbd754f82942d09b
Kari Marttila <kari.marttila@iki.fi>
2025-02-18 19:48:01 +0200

    WIP: ajax call

--- webstore-demo/store2/src/cljs/frontend/core.cljs
+++ webstore-demo/store2/src/cljs/frontend/core.cljs
@@ -1,7 +1,27 @@
 (ns frontend.core
   (:require [uix.core :as uix :refer [defui $]]
-            [uix.dom]))
+            [uix.dom]
+            [ajax.core :refer [GET POST]]
+            [cljs.core :refer [js->clj]]
+            ))
 
+
+(defn handle-books-click []
+  ; OK: GET "/api/products/books"  ; => On Consolessa root/arr/0/arr/0 alla...
+  (GET "/api/products/books/2005"
+    {:handler (fn [response]
+                (let [data (js->clj response :keywordize-keys true)]
+                  (js/console.log "Response data:" response)))
+     :error-handler (fn [error]
+                      (js/console.error "Error fetching books:" error))}))
+
+
+#_(defn handle-books-click []
+  (GET "/api/products/books"
+    {:handler (fn [response]
+                (js/console.log "Books data:" response))
+     :error-handler (fn [error]
+                      (js/console.error "Error fetching books:" error))}))
 
 (defui app []
   ($ :div.flex.justify-center.h-screen
@@ -10,18 +30,10 @@
         ($ :div.flex.flex-col.mt-4
            ($ :span.text-3xl.text-blue-800 "Browse products:")
            ($ :div.flex.flex-col.items-center.mt-4
-              ($ :a.mt-2.text-2xl.text-blue-700 {:href "#"} "Books")
+              ($ :a.mt-2.text-2xl.text-blue-700 {:href "#" :onClick handle-books-click} "Books")
               ($ :a.mt-2.text-2xl.text-blue-700 {:href "#"} "Movies"))))))
 
-#_(defui app []
-  ($ :div.flex.justify-center.h-screen 
-     ($ :h1.text-blue-700.mt-4 "Web Store 2")
-     
-     ($ :a.text-blue-500.mt-2 {:href "#"} "Books")
-     
-     ($ :a.text-blue-500.mt-2 {:href "#"} "Movies")
-     
-     ))
+
 
 (defonce root
   ; NOTE: This id needs to be the same as in routes.clj => index => [:div#store2app
