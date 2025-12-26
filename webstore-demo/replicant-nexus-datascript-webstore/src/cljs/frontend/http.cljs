16f277529ce41f8fd4c642f44831b35a7708f469
Kari Marttila <kari.marttila@iki.fi>
2025-12-26 11:56:19 +0200

    post function in http.cljs

--- webstore-demo/replicant-nexus-datascript-webstore/src/cljs/frontend/http.cljs
+++ webstore-demo/replicant-nexus-datascript-webstore/src/cljs/frontend/http.cljs
@@ -8,7 +8,6 @@
                [frontend.util :as f-util]))
 
 
-
 (defn fetch [system action]
   (when goog.DEBUG (f-util/clog "fetch, action: " action))
   (let [pg (:pg action)
@@ -26,43 +25,7 @@
             (when goog.DEBUG (f-util/clog "fetch, error: " (.-message e))))))))
 
 
-#_(defn fetch [system action]
-  (when goog.DEBUG (f-util/clog "fetch, action: " action))
-  (let [pg (:pg action)
-        url (str "/api" (get-in action [:query :api]))]
-    (go (try
-          (let [response (<! (http/get url))]
-            (if (= 200 (:status response))
-              (let [data (:body response)]
-                (when goog.DEBUG (f-util/clog "fetch, data: " data))
-                )
-              #_(dispatcher nil [[:db/assoc-in [:db/data pg] (:body response)]])
-              #_(dispatcher nil [[:db/assoc-in [:db/data pg] {:error (:status response)
-                                                              :pg pg}]])))
-          (catch js/Error e
-            (when goog.DEBUG (f-util/clog "fetch, error: " (.-message e)))
-            #_(dispatcher nil [[:db/assoc :db/data {:error (.-message e)
-                                                  :pg pg}]
-                             [:route/home]]))))))
-
-
-
-(comment 
-  
-
-  
-  (let [action {:query {:id :books, :api "/products/books"}, :pg :books}]
-    ;; NOTE: You get the logging in the browser Console! (not in REPL window!)
-    (fetch :dummy action))
-  
-  
-  
-  
-  
-  )
-
-
-(defn post [dispatcher action]
+(defn post [system action]
   (when goog.DEBUG (f-util/clog "post, action: " action))
   (let [product (:product action)
         pg (:pg action)
@@ -70,23 +33,24 @@
     (go (try
           (let [response (<! (http/post url {:json-params product}))]
             (if (= 200 (:status response))
-              (dispatcher nil [[:db/assoc :db/product-created {:product (:body response)
-                                                               :pg pg}]
-                               [:db/dissoc :db/new-product]
-                               ; Fetch new products (since added one).
-                               [:backend/fetch {:query (:query action)
-                                                :pg pg}]
-                               [:route/home]])
-              (dispatcher nil [[:db/assoc :db/product-created {:error (:status response)
-                                                               :pg pg}]
-                               [:db/dissoc :db/new-product]
-                               [:route/home]])))
+              ;; Success - dispatch actions to add product and navigate home
+              (nxr/dispatch system nil [[:add/product {:product (:body response) :pg pg}]
+                                        [:route/home]])
+              ;; Error - dispatch action to show error
+              (nxr/dispatch system nil [[:db/assoc :db/product-created {:error (:status response) :pg pg}]
+                                        [:route/home]])))
           (catch js/Error e
-            (dispatcher nil [[:db/assoc :db/product-created {:error (.-message e)
-                                                             :pg pg}]
-                             [:db/dissoc :db/new-product]
-                             [:route/home]]))))))
+            ;; Exception - dispatch action to show error
+            (nxr/dispatch system nil [[:db/assoc :db/product-created {:error (.-message e) :pg pg}]
+                                      [:route/home]]))))))
 
+
+
+(comment
+
+  (let [action {:query {:id :books, :api "/products/books"}, :pg :books}]
+    ;; NOTE: You get the logging in the browser Console! (not in REPL window!)
+    (fetch :dummy action)))
 
 (comment
 
@@ -97,7 +61,7 @@
           (def failed-query (:status response)))))
   ok-query
   failed-query
-
+  
   (def my-book  {:author "Kari Marttila",
                  :country "Finland",
                  ; :id 2034,  => No :id when posting!!!
@@ -106,12 +70,15 @@
                  :product-group 1,
                  :title "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
                  :year 1997})
+  ;;=> #'frontend.http/my-book
 
   (go (let [response (<! (http/post "/api/products/books" {:json-params my-book}))]
         (if (= 200 (:status response))
           (def ok-post (:body response))
           (do (def my-response response)
               (def failed-post (:status response))))))
+  ;;=> #object [cljs.core.async.impl.channels.ManyToManyChannel]
+  ;;=> nil
 
   (def my-movie {:product-group 2, :title "Juurakon Hulda", :price 82.92, :director "Valentin Vaala", :year 1937, :country "Finland", :genre "Drama"})
   (go (let [response (<! (http/post "/api/products/movies" {:json-params my-movie}))]
